6052bdaee8f5710028da95b901c82a2d97fe7321
Eric Hamilton <erickenneth@comcast.net>
2023-12-18 15:35:38 -0800

    Update backup

--- backup/backup.py
+++ backup/backup.py
@@ -10,35 +10,43 @@ def read_config(config_file):
     config.read(config_file)
     backupables = {}
     for section in config.sections():
-        item = section
         source_path = config.get(section, "source")
-        dest_paths = json.loads(config.get(section,"destinations"))
-        backupables[item] = {
-            "source_path":source_path,
-            "destinations":dest_paths
+        item_type = config.get(section, "type")
+        dest_paths = json.loads(config.get(section, "destinations"))
+        backupables[section] = {
+            "source_path": source_path,
+            "destinations": dest_paths,
+            "item_type": item_type
         }
     return backupables
 
+def backup_item(source, dest, item_type):
+    if os.path.isdir(dest):
+        if item_type == "file":
+            shutil.copy2(source, dest)
+        elif item_type == "folder":
+            shutil.copytree(source, dest)
+    else:
+        return f"Could not find dest: {dest} (source = {source})"
+
 def main(backupables):
     fails = []
-    for backup in backupables:
-        backup = backupables[backup]
+    for name, backup in backupables.items():
         source = backup["source_path"]
-        if os.path.isfile(source):
+        item_type = backup["item_type"]
+        if os.path.exists(source):
             for dest in backup["destinations"]:
-                if os.path.isdir(dest):
-                    shutil.copy2(source, dest)
-                else:
-                    fails.append(f"Could not find dest: {dest} (source = {source})")
+                fail_message = backup_item(source, dest, item_type)
+                if fail_message:
+                    fails.append(fail_message)
         else:
             fails.append(f"Could not find source: {source}")
     return fails
 
 if __name__ == '__main__':
-    config_path = Path(os.path.dirname(os.path.abspath(sys.argv[0]))).parent.absolute().joinpath("data/backup.ini")
+    script_dir = Path(__file__).resolve().parent
+    config_path = script_dir.joinpath("data/backup.ini")
     backupables = read_config(config_path)
     fails = main(backupables)
-    if fails:
-        for f in fails:
-            print(f)
-
+    for fail in fails:
+        print(fail)
